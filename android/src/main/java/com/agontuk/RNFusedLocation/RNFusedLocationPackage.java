c98d809f5f20998935fa65f04a900bcc2ab34419
Iftekhar Rifat <rifat662@gmail.com>
2018-01-11 13:55:09 +0600

    Fix import

--- android/src/main/java/com/agontuk/RNFusedLocation/RNFusedLocationPackage.java
+++ android/src/main/java/com/agontuk/RNFusedLocation/RNFusedLocationPackage.java
@@ -1,10 +1,12 @@
 package com.agontuk.RNFusedLocation;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
