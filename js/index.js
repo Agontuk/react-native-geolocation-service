2f3b07d9dac10001079cdbd55fc6fea45bdfb239
Iftekhar Rifat <rifat662@gmail.com>
2018-01-14 17:57:42 +0600

    Remove console statement from setRNConfiguration & requestAuthorization

--- js/index.js
+++ js/index.js
@@ -6,15 +6,9 @@ const noop = () => {};
 
 // TODO: add static type checker
 const Geolocation = Platform.OS === 'ios' ? global.navigator.geolocation : {
-    setRNConfiguration: (config) => { // eslint-disable-line no-unused-vars
-        // eslint-disable-next-line no-console
-        console.warn('setRNConfiguration is not supported on android');
-    },
+    setRNConfiguration: (config) => {}, // eslint-disable-line no-unused-vars
 
-    requestAuthorization: () => {
-        // eslint-disable-next-line no-console
-        console.warn('requestAuthorization is not supported on android');
-    },
+    requestAuthorization: () => {},
 
     getCurrentPosition: async (success, error = noop, options = {}) => {
         // Right now, we're assuming user already granted location permission.
