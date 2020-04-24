25920c06f0ce4f76eeff1bcc4ce25e723b8ebe08
Iftekhar Rifat <rifat662@gmail.com>
2020-04-24 17:24:35 +0600

    Implement startObserving & stopObserving methods

--- ios/RNFusedLocation.swift
+++ ios/RNFusedLocation.swift
@@ -1,6 +1,15 @@
 import Foundation
 import CoreLocation
 
+let DEFAULT_ACCURACY: CLLocationAccuracy = kCLLocationAccuracyHundredMeters
+let DEFAULT_DISTANCE_FILTER: CLLocationDistance = 100
+
+enum LocationError: Int {
+  case PERMISSION_DENIED = 1
+  case POSITION_UNAVAILABLE
+  case TIMEOUT
+}
+
 enum AuthorizationStatus: String {
   case disabled, granted, denied, restricted
 }
@@ -8,7 +17,10 @@ enum AuthorizationStatus: String {
 @objc(RNFusedLocation)
 class RNFusedLocation: RCTEventEmitter {
   private let locationManager: CLLocationManager = CLLocationManager()
+  private var hasListeners: Bool = false
+  private var observing: Bool = false
   private var resolveAuthorizationStatus: RCTPromiseResolveBlock? = nil
+  private var useSignificantChanges: Bool = false
 
   override init() {
     super.init()
@@ -16,6 +28,14 @@ class RNFusedLocation: RCTEventEmitter {
   }
 
   deinit {
+    if observing {
+      useSignificantChanges
+        ? locationManager.stopMonitoringSignificantLocationChanges()
+        : locationManager.stopUpdatingLocation()
+
+      observing = false
+    }
+
     locationManager.delegate = nil;
   }
 
@@ -27,6 +47,16 @@ class RNFusedLocation: RCTEventEmitter {
   // MARK: RCTEventEmitter
   override func supportedEvents() -> [String]! {
     return ["geolocationDidChange", "geolocationError"]
+  }
+
+  // MARK: RCTEventEmitter
+  override func startObserving() -> Void {
+    hasListeners = true
+  }
+
+  // MARK: RCTEventEmitter
+  override func stopObserving() -> Void {
+    hasListeners = false
   }
 
   // MARK: Bridge Method
@@ -62,12 +92,28 @@ class RNFusedLocation: RCTEventEmitter {
 
   // MARK: Bridge Method
   @objc func startLocationUpdate(_ options: [String: Any]) -> Void {
-    NSLog("startLocationUpdate")
+    let distanceFilter = options["distanceFilter"] as? Double ?? DEFAULT_DISTANCE_FILTER
+    let highAccuracy = options["enableHighAccuracy"] as? Bool ?? false
+    let significantChanges = options["useSignificantChanges"] as? Bool ?? false
+
+    locationManager.desiredAccuracy = highAccuracy ? kCLLocationAccuracyBest : DEFAULT_ACCURACY
+    locationManager.distanceFilter = distanceFilter
+
+    significantChanges
+      ? locationManager.startMonitoringSignificantLocationChanges()
+      : locationManager.startUpdatingLocation()
+
+    useSignificantChanges = significantChanges
+    observing = true
   }
 
   // MARK: Bridge Method
   @objc func stopLocationUpdate() -> Void {
-    NSLog("stopLocationUpdate")
+    useSignificantChanges
+      ? locationManager.stopMonitoringSignificantLocationChanges()
+      : locationManager.stopUpdatingLocation()
+
+    observing = false
   }
 
   private func checkPlistKeys(authorizationLevel: String) -> Void {
@@ -110,5 +156,66 @@ extension RNFusedLocation: CLLocationManagerDelegate {
     }
 
     resolveAuthorizationStatus = nil
+  }
+
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    guard let location: CLLocation = locations.last else { return }
+    let locationData: [String: Any] = [
+      "coords": [
+        "latitude": location.coordinate.latitude,
+        "longitude": location.coordinate.longitude,
+        "altitude": location.altitude,
+        "accuracy": location.horizontalAccuracy,
+        "altitudeAccuracy": location.verticalAccuracy,
+        "heading": location.course,
+        "speed": location.speed
+      ],
+      "timestamp": location.timestamp.timeIntervalSince1970 * 1000 // ms
+    ]
+
+
+    if hasListeners && observing {
+      sendEvent(withName: "geolocationDidChange", body: locationData)
+    }
+  }
+
+  func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+    let errorData: [String: Any]
+
+    if let clErr = error as? CLError {
+      switch clErr.code {
+        case CLError.denied:
+          if !CLLocationManager.locationServicesEnabled() {
+            errorData = [
+              "code": LocationError.POSITION_UNAVAILABLE.rawValue,
+              "message": "Location service is turned off"
+            ]
+          } else {
+            errorData = [
+              "code": LocationError.PERMISSION_DENIED.rawValue,
+              "message": "Location permission denied"
+            ]
+          }
+        case CLError.network:
+          errorData = [
+            "code": LocationError.POSITION_UNAVAILABLE.rawValue,
+            "message": "Unable to retrieve location due to a network failure"
+          ]
+        default:
+          errorData = [
+            "code": LocationError.POSITION_UNAVAILABLE.rawValue,
+            "message": "Unable to retrieve location"
+          ]
+      }
+    } else {
+      errorData = [
+        "code": LocationError.POSITION_UNAVAILABLE.rawValue,
+        "message": "Unable to retrieve location"
+      ]
+    }
+
+    if hasListeners && observing {
+      sendEvent(withName: "geolocationError", body: errorData)
+    }
   }
 }
