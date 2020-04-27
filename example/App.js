fe42f5858fb145a26c8e5add64ae52ba1e3eab64
Iftekhar Rifat <rifat662@gmail.com>
2020-04-27 19:41:37 +0600

    Update example project

--- example/App.js
+++ example/App.js
@@ -6,15 +6,18 @@
 
 import React, { Component } from 'react';
 import {
+  Alert,
   Button,
+  Linking,
   PermissionsAndroid,
   Platform,
   StyleSheet,
   Text,
   ToastAndroid,
-  View
+  View,
 } from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
+import appConfig from './app.json';
 
 export default class App extends Component<{}> {
   watchId = null;
@@ -22,40 +25,86 @@ export default class App extends Component<{}> {
   state = {
     loading: false,
     updatesEnabled: false,
-    location: {}
+    location: {},
+  };
+
+  hasLocationPermissionIOS = async () => {
+    const openSetting = () => {
+      Linking.openSettings().catch(() => {
+        Alert.alert('Unable to open settings');
+      });
+    };
+    const status = await Geolocation.requestAuthorization('whenInUse');
+
+    if (status === 'granted') {
+      return true;
+    }
+
+    if (status === 'denied') {
+      Alert.alert('Location permission denied');
+    }
+
+    if (status === 'disabled') {
+      Alert.alert(
+        `Turn on Location Services to allow "${appConfig.displayName}" to determine your location.`,
+        '',
+        [
+          { text: 'Go to Settings', onPress: openSetting },
+          { text: "Don't Use Location", onPress: () => {} },
+        ],
+      );
+    }
+
+    return false;
   };
 
   hasLocationPermission = async () => {
-    if (Platform.OS === 'ios' ||
-        (Platform.OS === 'android' && Platform.Version < 23)) {
+    if (Platform.OS === 'ios') {
+      const hasPermission = await this.hasLocationPermissionIOS();
+      return hasPermission;
+    }
+
+    if (Platform.OS === 'android' && Platform.Version < 23) {
       return true;
     }
 
     const hasPermission = await PermissionsAndroid.check(
-      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
     );
 
-    if (hasPermission) return true;
+    if (hasPermission) {
+      return true;
+    }
 
     const status = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
     );
 
-    if (status === PermissionsAndroid.RESULTS.GRANTED) return true;
+    if (status === PermissionsAndroid.RESULTS.GRANTED) {
+      return true;
+    }
 
     if (status === PermissionsAndroid.RESULTS.DENIED) {
-      ToastAndroid.show('Location permission denied by user.', ToastAndroid.LONG);
+      ToastAndroid.show(
+        'Location permission denied by user.',
+        ToastAndroid.LONG,
+      );
     } else if (status === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN) {
-      ToastAndroid.show('Location permission revoked by user.', ToastAndroid.LONG);
+      ToastAndroid.show(
+        'Location permission revoked by user.',
+        ToastAndroid.LONG,
+      );
     }
 
     return false;
-  }
+  };
 
   getLocation = async () => {
     const hasLocationPermission = await this.hasLocationPermission();
 
-    if (!hasLocationPermission) return;
+    if (!hasLocationPermission) {
+      return;
+    }
 
     this.setState({ loading: true }, () => {
       Geolocation.getCurrentPosition(
@@ -67,15 +116,23 @@ export default class App extends Component<{}> {
           this.setState({ location: error, loading: false });
           console.log(error);
         },
-        { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000, distanceFilter: 50, forceRequestLocation: true }
+        {
+          enableHighAccuracy: true,
+          timeout: 2000,
+          maximumAge: 10000,
+          distanceFilter: 0,
+          forceRequestLocation: true,
+        },
       );
     });
-  }
+  };
 
   getLocationUpdates = async () => {
     const hasLocationPermission = await this.hasLocationPermission();
 
-    if (!hasLocationPermission) return;
+    if (!hasLocationPermission) {
+      return;
+    }
 
     this.setState({ updatesEnabled: true }, () => {
       this.watchId = Geolocation.watchPosition(
@@ -87,30 +144,48 @@ export default class App extends Component<{}> {
           this.setState({ location: error });
           console.log(error);
         },
-        { enableHighAccuracy: true, distanceFilter: 0, interval: 5000, fastestInterval: 2000 }
+        {
+          enableHighAccuracy: true,
+          distanceFilter: 50,
+          interval: 5000,
+          fastestInterval: 2000,
+          forceRequestLocation: true,
+        },
       );
     });
-  }
+  };
 
   removeLocationUpdates = () => {
-      if (this.watchId !== null) {
-          Geolocation.clearWatch(this.watchId);
-          this.setState({ updatesEnabled: false })
-      }
-  }
+    if (this.watchId !== null) {
+      Geolocation.clearWatch(this.watchId);
+      this.setState({ updatesEnabled: false });
+    }
+  };
 
   render() {
     const { loading, location, updatesEnabled } = this.state;
     return (
       <View style={styles.container}>
-        <Button title='Get Location' onPress={this.getLocation} disabled={loading || updatesEnabled} />
+        <Button
+          title="Get Location"
+          onPress={this.getLocation}
+          disabled={loading || updatesEnabled}
+        />
         <View style={styles.buttons}>
-            <Button title='Start Observing' onPress={this.getLocationUpdates} disabled={updatesEnabled} />
-            <Button title='Stop Observing' onPress={this.removeLocationUpdates} disabled={!updatesEnabled} />
+          <Button
+            title="Start Observing"
+            onPress={this.getLocationUpdates}
+            disabled={updatesEnabled}
+          />
+          <Button
+            title="Stop Observing"
+            onPress={this.removeLocationUpdates}
+            disabled={!updatesEnabled}
+          />
         </View>
 
         <View style={styles.result}>
-            <Text>{JSON.stringify(location, null, 4)}</Text>
+          <Text>{JSON.stringify(location, null, 4)}</Text>
         </View>
       </View>
     );
@@ -123,19 +198,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-    paddingHorizontal: 12
+    paddingHorizontal: 12,
   },
   result: {
-      borderWidth: 1,
-      borderColor: '#666',
-      width: '100%',
-      paddingHorizontal: 16
+    borderWidth: 1,
+    borderColor: '#666',
+    width: '100%',
+    paddingHorizontal: 16,
   },
   buttons: {
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      alignItems: 'center',
-      marginVertical: 12,
-      width: '100%'
-  }
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginVertical: 12,
+    width: '100%',
+  },
 });
