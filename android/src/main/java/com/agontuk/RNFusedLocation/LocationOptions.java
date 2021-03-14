2bdd835f7321bb367a1cdb8bddd36a35ab1cf396
Iftekhar Rifat <rifat662@gmail.com>
2021-03-15 03:37:51 +0600

    Android: add LocationOptions class to parse options

---
+++ android/src/main/java/com/agontuk/RNFusedLocation/LocationOptions.java
@@ -0,0 +1,145 @@
+package com.agontuk.RNFusedLocation;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableType;
+
+public class LocationOptions {
+  private static final float DEFAULT_DISTANCE_FILTER = 100;
+  private static final long DEFAULT_INTERVAL = 10 * 1000;  /* 10 secs */
+  private static final long DEFAULT_FASTEST_INTERVAL = 5 * 1000; /* 5 sec */
+
+  private final LocationAccuracy accuracy;
+  private final long interval;
+  private final long fastestInterval;
+  private final float distanceFilter;
+  private final long timeout;
+  private final double maximumAge;
+  private final boolean showLocationDialog;
+  private final boolean forceRequestLocation;
+
+  private LocationOptions(
+    LocationAccuracy accuracy,
+    long interval,
+    long fastestInterval,
+    float distanceFilter,
+    long timeout,
+    double maximumAge,
+    boolean showLocationDialog,
+    boolean forceRequestLocation
+  ) {
+    this.accuracy = accuracy;
+    this.interval = interval;
+    this.fastestInterval = fastestInterval;
+    this.distanceFilter = distanceFilter;
+    this.timeout = timeout;
+    this.maximumAge = maximumAge;
+    this.showLocationDialog = showLocationDialog;
+    this.forceRequestLocation = forceRequestLocation;
+  }
+
+  public static LocationOptions fromReadableMap(ReadableMap map) {
+    LocationAccuracy accuracy = getAccuracy(map);
+    long interval = map.hasKey("interval")
+      ? (long) map.getDouble("interval")
+      : DEFAULT_INTERVAL;
+    long fastestInterval = map.hasKey("fastestInterval")
+      ? (long) map.getDouble("fastestInterval")
+      : DEFAULT_FASTEST_INTERVAL;
+    float distanceFilter = map.hasKey("distanceFilter")
+      ? (float) map.getDouble("distanceFilter")
+      : DEFAULT_DISTANCE_FILTER;
+    long timeout = map.hasKey("timeout")
+      ? (long) map.getDouble("timeout")
+      : Long.MAX_VALUE;
+    double maximumAge = map.hasKey("maximumAge")
+      ? map.getDouble("maximumAge")
+      : Double.POSITIVE_INFINITY;
+    boolean showLocationDialog =
+      !map.hasKey("showLocationDialog") || map.getBoolean("showLocationDialog");
+    boolean forceRequestLocation =
+      map.hasKey("forceRequestLocation") && map.getBoolean("forceRequestLocation");
+
+
+    return new LocationOptions(
+      accuracy,
+      interval,
+      fastestInterval,
+      distanceFilter,
+      timeout,
+      maximumAge,
+      showLocationDialog,
+      forceRequestLocation
+    );
+  }
+
+  public LocationAccuracy getAccuracy() {
+    return accuracy;
+  }
+
+  public long getInterval() {
+    return interval;
+  }
+
+  public long getFastestInterval() {
+    return fastestInterval;
+  }
+
+  public float getDistanceFilter() {
+    return distanceFilter;
+  }
+
+  public long getTimeout() {
+    return timeout;
+  }
+
+  public double getMaximumAge() {
+    return maximumAge;
+  }
+
+  public boolean isShowLocationDialog() {
+    return showLocationDialog;
+  }
+
+  public boolean isForceRequestLocation() {
+    return forceRequestLocation;
+  }
+
+  /**
+   * Determine location priority from user provided accuracy level
+   */
+  private static LocationAccuracy getAccuracy(@NonNull ReadableMap options) {
+    String accuracy = "";
+    boolean highAccuracy = options.hasKey("enableHighAccuracy")
+      && options.getBoolean("enableHighAccuracy");
+
+    if (options.hasKey("accuracy") && options.getType("accuracy") == ReadableType.Map) {
+      ReadableMap accuracyMap = options.getMap("accuracy");
+
+      if (accuracyMap != null &&
+        accuracyMap.hasKey("android") &&
+        accuracyMap.getType("android") == ReadableType.String
+      ) {
+        String value = accuracyMap.getString("android");
+
+        if (value != null) {
+          accuracy = value;
+        }
+      }
+    }
+
+    switch (accuracy) {
+      case "high":
+        return LocationAccuracy.high;
+      case "balanced":
+        return LocationAccuracy.balanced;
+      case "low":
+        return LocationAccuracy.low;
+      case "passive":
+        return LocationAccuracy.passive;
+      default:
+        return highAccuracy ? LocationAccuracy.high : LocationAccuracy.balanced;
+    }
+  }
+}
