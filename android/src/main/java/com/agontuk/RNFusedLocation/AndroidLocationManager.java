50e5082c32b72c10a6a640f002cb80b869da6342
Iftekhar Rifat <rifat662@gmail.com>
2021-03-15 20:15:38 +0600

    Android: basic implementation of fused & android location manager

---
+++ android/src/main/java/com/agontuk/RNFusedLocation/AndroidLocationManager.java
@@ -0,0 +1,27 @@
+package com.agontuk.RNFusedLocation;
+
+public class AndroidLocationManager implements LocationManager {
+  public AndroidLocationManager() {
+    //
+  }
+
+  @Override
+  public void getCurrentLocation(LocationOptions locationOptions, LocationListener locationListener) {
+    //
+  }
+
+  @Override
+  public boolean onActivityResult(int requestCode, int resultCode) {
+    return true;
+  }
+
+  @Override
+  public void requestLocationUpdates(LocationOptions locationOptions, LocationListener locationListener) {
+    //
+  }
+
+  @Override
+  public void removeLocationUpdates() {
+    //
+  }
+}
---
+++ android/src/main/java/com/agontuk/RNFusedLocation/FusedLocationManager.java
@@ -0,0 +1,27 @@
+package com.agontuk.RNFusedLocation;
+
+public class FusedLocationManager implements LocationManager {
+  public FusedLocationManager() {
+    //
+  }
+
+  @Override
+  public void getCurrentLocation(LocationOptions locationOptions, LocationListener locationListener) {
+    //
+  }
+
+  @Override
+  public boolean onActivityResult(int requestCode, int resultCode) {
+    return true;
+  }
+
+  @Override
+  public void requestLocationUpdates(LocationOptions locationOptions, final LocationListener locationListener) {
+    //
+  }
+
+  @Override
+  public void removeLocationUpdates() {
+    //
+  }
+}
